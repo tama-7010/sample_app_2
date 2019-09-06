7424a3026acaedd1ad7233e30b17021fb228a730
tama-7010 <otama.engine@gmail.com>
2019-09-06 23:01:49 +0900

    Add partials and scss

--- spec/request/static_pages_controller_spec.rb
+++ spec/request/static_pages_controller_spec.rb
@@ -7,30 +7,25 @@ describe StaticPagesController do
 
   describe 'static pages', type: :feature do
     it 'should get root' do
-      visit root_url
-      expect(page).to have_http_status(:ok)
-    end
-
-    it 'should get home' do
-      visit static_pages_home_url
+      visit root_path
       expect(page).to have_http_status(:ok)
       expect(page).to have_title("#{@base_title}")
     end
 
     it 'should get help' do
-      visit static_pages_help_url
+      visit help_path
       expect(page).to have_http_status(:ok)
       expect(page).to have_title("Help | #{@base_title}")
     end
 
     it 'should get about' do
-      visit static_pages_about_url
+      visit about_path
       expect(page).to have_http_status(:ok)
       expect(page).to have_title("About | #{@base_title}")
     end
 
     it 'should get contact' do
-      visit static_pages_contact_url
+      visit contact_path
       expect(page).to have_http_status(:ok)
       expect(page).to have_title("Contact | #{@base_title}")
     end
